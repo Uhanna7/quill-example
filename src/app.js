82faaeb4afe1833d5e26ab663ddd0381428931d6
Hanna Uri <101406038+Uhanna7@users.noreply.github.com>
2024-04-16 12:04:34 +0200

    basic quill setup

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import Quill from 'quill';
+// Or if you only need the core build
+// import Quill from 'quill/core';
+
+import "quill/dist/quill.snow.css";
+
+const editor = new Quill('#editor', {
+    theme: 'snow',
+    modules: {
+      toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        ['blockquote', 'code-block'],
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+        [{ 'script': 'sub'}, { 'script': 'super' }],
+        [{ 'indent': '-1'}, { 'indent': '+1' }],
+        [{ 'direction': 'rtl' }],
+        [{ 'size': ['small', false, 'large', 'huge'] }],
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'color': [] }, { 'background': [] }],
+        [{ 'font': [] }],
+        ['link', 'image', 'video'],
+        ['clean']
+      ]
+    }
+});
